03b9fad6dc54cbdcf7d9d07963962eec4a7f0992
GaneshRoyal07 <kondetiganesh43@gmail.com>
2026-03-16 09:32:03 +0530

    UI Components-1

---
+++ src/components/UserTable.js
@@ -0,0 +1,33 @@
+import React from "react";
+import users from "../data/users";
+
+function UserTable() {
+  return (
+    <div>
+      <h2>User Details</h2>
+
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </div>
+  );
+}
+
+export default UserTable;
---
+++ src/data/users.js
@@ -0,0 +1,7 @@
+const users = [
+  { id: 1, name: "Ganesh", email: "kondetiganesh07@gmail.com" },
+  { id: 2, name: "Siva", email: "sivanalabala12@gmail.com" },
+  { id: 3, name: "Sasi", email: "nanabalasasi456@gmail.com" }
+];
+
+export default users;
